54a3302ee3370d4e9695a680e39fd02fe09569a2
Ikerrom <ikerrome1@gmail.com>
2023-10-10 14:30:39 +0200

    un espacio

--- view/index.php
+++ view/index.php
@@ -2,19 +2,19 @@
     include "../template/header.php";
     require_once '../controller/curso-controller.php';
     $cursoController = new CursoController();
-    
     $filternombre = "";
 
     if (isset($_POST['filter'])) {
         $filternombre = $_POST['nombre'];
     }
+    
     $cursos = $cursoController->selectCursos('nombre',$filternombre,"index.php");
 ?>
 <body>
     <div class="body">
         <p style="font-size:2vw;">CURSOS</p>
         <br>
-        <table >
+        <table>
             <?php
                 if (!empty($cursos)) {
                 foreach ($cursos as $curso) {
